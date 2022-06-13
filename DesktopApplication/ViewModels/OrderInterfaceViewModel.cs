12fa45c0d32493322c701114b5acdb83f5ad00b7
Abcdavid00 <20521470@gm.uit.edu.vn>
2022-06-14 01:21:04 +0700

    Staff View Foundation

---
+++ DesktopApplication/ViewModels/OrderInterfaceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CSWBManagementApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSWBManagementApplication.ViewModels
+{
+    internal class OrderInterfaceViewModel : ViewModelBase
+    {
+        public OrderInterfaceViewModel(Staff staff)
+        {
+        }
+    }
+}
--- DesktopApplication/ViewModels/StaffViewModel.cs
+++ DesktopApplication/ViewModels/StaffViewModel.cs
@@ -1,17 +1,76 @@
-﻿namespace CSWBManagementApplication.ViewModels
+﻿using CSWBManagementApplication.Commands;
+using CSWBManagementApplication.Models;
+using CSWBManagementApplication.Service;
+using System.Windows.Input;
+
+namespace CSWBManagementApplication.ViewModels
 {
     internal class StaffViewModel : ViewModelBase
     {
-        public string ViewName
+        private OrderInterfaceViewModel orderInterfaceViewModel;
+        public OrderInterfaceViewModel OrderInterfaceViewModel
         {
-            get => "StaffView";
+            get => orderInterfaceViewModel;
+            set
+            {
+                orderInterfaceViewModel = value;
+                OnPropertyChanged();
+            }
         }
+
+        private StaffProfileViewModel staffProfileViewModel;
+        public StaffProfileViewModel StaffProfileViewModel
+        {
+            get => staffProfileViewModel;
+            set
+            {
+                staffProfileViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Staff staff;
 
         private MainViewModel mainViewModel;
 
         public StaffViewModel(MainViewModel mainViewModel)
         {
             this.mainViewModel = mainViewModel;
+            this.staff = this.mainViewModel.CurrentUser as Staff;
+            OrderInterfaceViewModel = new OrderInterfaceViewModel(this.staff);
+            StaffProfileViewModel = new StaffProfileViewModel(this.staff);
+        }
+
+        public ICommand LogoutCommand
+        {
+            get => new CommandBase(() => { mainViewModel.UserLink = null; });
+        }
+
+        private int selectedIndex;
+        public int SelectedIndex
+        {
+            get => selectedIndex;
+            set
+            {
+                selectedIndex = MiscFunctions.CappedSetter(value,0,1);
+                OnPropertyChanged(nameof(IsOrderSelected));
+                OnPropertyChanged(nameof(IsStaffProfileSelected));
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsOrderSelected { get => SelectedIndex == 0; }     
+
+        public ICommand OrderButtonPressed
+        {
+            get => new CommandBase(() => { SelectedIndex=0; });
+        }
+
+        public bool IsStaffProfileSelected { get => SelectedIndex == 1; }
+
+        public ICommand StaffProfileButtonPressed
+        {
+            get => new CommandBase(() => { SelectedIndex = 1; });
         }
     }
 }
